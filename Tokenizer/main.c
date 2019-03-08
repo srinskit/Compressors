587ae3115e3ef6f8f104905c818e18700d3dcc21
srinskit <srinagrao2007@gmail.com>
2019-03-08 21:16:37 +0530

    Implemented ASCII & English support

--- Tokenizer/main.c
+++ Tokenizer/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctype.h>
+
 #define FILENAME_SIZE 50
 extern char *optarg;
 
 int main(int argc, char *const argv[]) {
-    enum lang {Bytes, English,};
-    enum lang ip_lang = Bytes;
+    enum lang {ASCII, English,};
+    enum lang ip_lang = ASCII;
     char ip_file[FILENAME_SIZE], op_file[FILENAME_SIZE];
     memset(ip_file, '\0', sizeof(ip_file));
     memset(op_file, '\0', sizeof(op_file));
@@ -17,8 +19,8 @@ int main(int argc, char *const argv[]) {
             case 'l':
                 if(strcmp(optarg, "English") == 0)
                     ip_lang = English;
-                else if (strcmp(optarg, "Bytes") == 0)
-                    ip_lang = Bytes;                
+                else if (strcmp(optarg, "ASCII") == 0)
+                    ip_lang = ASCII;                
                 break;
             case 'i':
                 snprintf(ip_file, sizeof(ip_file), "%s", optarg);
@@ -38,12 +40,36 @@ int main(int argc, char *const argv[]) {
         printf("Cannot open output file\n");
         return 1;
     }
-    fprintf(op_fptr, "%s\n", "Hello World");
-    if(ip_lang == Bytes){
-        
+    if(ip_lang == ASCII){
+        char ch;
+        while (fscanf(ip_fptr, "%c", &ch) != EOF){
+            fprintf(op_fptr, "%c\n", ch);
+        }
     }
     else if(ip_lang == English){
-        
+        char buffer[1000], ch;
+        int i = 0;
+        while (fscanf(ip_fptr, "%c", &ch) != EOF){
+            if(isalpha(ch)){
+                if(i < sizeof(buffer) - 1){
+                    buffer[i++] = ch;
+                }
+                else{
+                    buffer[sizeof(buffer) - 1] = '\0';
+                    fprintf(op_fptr, "%s\n", buffer);
+                    buffer[0] = ch;
+                    i = 1;
+                }
+            }
+            else if(i > 0){
+                buffer[i] = '\0';
+                fprintf(op_fptr, "%s\n%c\n", buffer, ch);
+                i = 0;
+            }
+            else{
+                fprintf(op_fptr, "%c\n", ch);
+            }
+        }
     }
     fclose(ip_fptr);
     fclose(op_fptr);
