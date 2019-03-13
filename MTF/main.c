f6940a4bb42259e7574a93338c4876ad2ae4e216
srinskit <srinagrao2007@gmail.com>
2019-03-13 11:03:14 +0530

    Cleaned, Formatted, Added default i/o

--- MTF/main.c
+++ MTF/main.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define FILENAME_SIZE 50
 extern char *optarg;
 
 int main(int argc, char *const argv[]) {
-    int encode = 0;
-    enum lang {ASCII, English,};
+    int encode = 1;
+    enum lang {
+        ASCII,
+        English,
+    };
     enum lang ip_lang = ASCII;
     char ip_file[FILENAME_SIZE], op_file[FILENAME_SIZE];
-    memset(ip_file, '\0', sizeof(ip_file));
-    memset(op_file, '\0', sizeof(op_file));
+    // Linux specific
+    snprintf(ip_file, sizeof(ip_file), "%s", "/dev/stdin");
+    snprintf(op_file, sizeof(op_file), "%s", "/dev/stdout");
     const char *optstring = "l:i:o:ed";
     int option;
-    while((option = getopt(argc, argv, optstring)) != -1){
-        switch(option){
+    while ((option = getopt(argc, argv, optstring)) != -1) {
+        switch (option) {
             case 'l':
-                if(strcmp(optarg, "English") == 0)
+                if (strcmp(optarg, "English") == 0)
                     ip_lang = English;
                 else if (strcmp(optarg, "ASCII") == 0)
-                    ip_lang = ASCII;                
+                    ip_lang = ASCII;
                 break;
             case 'i':
                 snprintf(ip_file, sizeof(ip_file), "%s", optarg);
@@ -36,108 +40,95 @@ int main(int argc, char *const argv[]) {
         }
     }
     FILE *ip_fptr, *op_fptr;
-    if((ip_fptr = fopen(ip_file,"r")) == NULL){
+    if ((ip_fptr = fopen(ip_file, "r")) == NULL) {
         printf("Cannot open input file\n");
         return 1;
     }
-    op_fptr = fopen(op_file,"w");
-    if((op_fptr = fopen(op_file,"w")) == NULL){
+    op_fptr = fopen(op_file, "w");
+    if ((op_fptr = fopen(op_file, "w")) == NULL) {
         printf("Cannot open output file\n");
         return 1;
     }
-    if(encode){
-        if(ip_lang == ASCII){
-            
-        }
-        else if(ip_lang == English){
+    if (encode) {
+        if (ip_lang == ASCII) {
+        } else if (ip_lang == English) {
             const int MAX_TOKEN_LENGTH = 100, STACK_SIZE = 100;
             int i, found;
             char *stack[STACK_SIZE];
             // token + \n + \0
             char line[MAX_TOKEN_LENGTH + 1 + 1];
-            for(i=0; i<STACK_SIZE; ++i)
-                stack[i] = NULL;
-            while(fgets(line, sizeof(line), ip_fptr) != NULL){
-                line[strlen(line)-1] = '\0';
-                if(strlen(line) == 0){
+            for (i = 0; i < STACK_SIZE; ++i) stack[i] = NULL;
+            while (fgets(line, sizeof(line), ip_fptr) != NULL) {
+                line[strlen(line) - 1] = '\0';
+                if (strlen(line) == 0) {
                     // When OG token is '\n'
                     // Todo: test when OG token is '\0'
-                    if(fgets(line, sizeof(line), ip_fptr) == NULL)
-                        break;
+                    if (fgets(line, sizeof(line), ip_fptr) == NULL) break;
                 }
                 char *prev_item = NULL;
                 found = 0;
-                for(i=0; i< STACK_SIZE && stack[i]!=NULL;++i){
-                    char* tmp = stack[i];
+                for (i = 0; i < STACK_SIZE && stack[i] != NULL; ++i) {
+                    char *tmp = stack[i];
                     stack[i] = prev_item;
                     prev_item = tmp;
-                    if(strcmp(line, prev_item) == 0){
+                    if (strcmp(line, prev_item) == 0) {
                         found = 1;
                         break;
                     }
                 }
-                if(found){
+                if (found) {
                     fprintf(op_fptr, "%d\n", i);
                     stack[0] = prev_item;
-                }
-                else{
-                    if(i < STACK_SIZE)
+                } else {
+                    if (i < STACK_SIZE)
                         stack[i] = prev_item;
                     else
                         free(prev_item);
                     fprintf(op_fptr, "%d\n%s\n", -1, line);
-                    stack[0] = (char*)malloc(MAX_TOKEN_LENGTH+1);
+                    stack[0] = (char *)malloc(MAX_TOKEN_LENGTH + 1);
                     strcpy(stack[0], line);
                 }
             }
-            for(i=0; i<STACK_SIZE; ++i)
-                free(stack[i]);
+            for (i = 0; i < STACK_SIZE; ++i) free(stack[i]);
         }
-    }
-    else{
-        if(ip_lang == ASCII){
-            
-        }
-        else if(ip_lang == English){
+    } else {
+        if (ip_lang == ASCII) {
+        } else if (ip_lang == English) {
             const int MAX_TOKEN_LENGTH = 100, STACK_SIZE = 100;
             int i, found;
             char *stack[STACK_SIZE];
             // token + \n + \0
             char line[MAX_TOKEN_LENGTH + 1 + 1];
-            for(i=0; i<STACK_SIZE; ++i)
-                stack[i] = NULL;
-            while(fgets(line, sizeof(line), ip_fptr) != NULL){
-                line[strlen(line)-1] = '\0';
-                if(strcmp(line, "-1") == 0){
-                    if(fgets(line, sizeof(line), ip_fptr) == NULL)
-                        break;
-                    line[strlen(line)-1] = '\0';
-                    if(strlen(line) == 0){
+            for (i = 0; i < STACK_SIZE; ++i) stack[i] = NULL;
+            while (fgets(line, sizeof(line), ip_fptr) != NULL) {
+                line[strlen(line) - 1] = '\0';
+                if (strcmp(line, "-1") == 0) {
+                    if (fgets(line, sizeof(line), ip_fptr) == NULL) break;
+                    line[strlen(line) - 1] = '\0';
+                    if (strlen(line) == 0) {
                         // When OG string is '\n\n'
                         // Todo: test when OG token is '\0'
-                        if(fgets(line, sizeof(line), ip_fptr) == NULL)
-                            break;
+                        if (fgets(line, sizeof(line), ip_fptr) == NULL) break;
                     }
-                    char* prev_item = NULL;
-                    for(i=0; i< STACK_SIZE && stack[i]!=NULL;++i){
-                        char* tmp = stack[i];
+                    char *prev_item = NULL;
+                    for (i = 0; i < STACK_SIZE && stack[i] != NULL; ++i) {
+                        char *tmp = stack[i];
                         stack[i] = prev_item;
                         prev_item = tmp;
                     }
-                    if(i< STACK_SIZE)
+                    if (i < STACK_SIZE)
                         stack[i] = prev_item;
                     else
                         free(prev_item);
-                    stack[0] = (char*)malloc(MAX_TOKEN_LENGTH+1);
+                    stack[0] = (char *)malloc(MAX_TOKEN_LENGTH + 1);
                     strcpy(stack[0], line);
                     fprintf(op_fptr, "%s\n", line);
-                }
-                else{
+                } else {
                     char *end;
                     long n = strtol(line, &end, 10);
                     char *prev_item = NULL;
-                    for(i=0; i<= n;++i){
-                        char* tmp = stack[i];
+                    for (i = 0; i <= n; ++i) {
+                        char *tmp = stack[i];
                         stack[i] = prev_item;
                         prev_item = tmp;
                     }
@@ -145,8 +136,7 @@ int main(int argc, char *const argv[]) {
                     fprintf(op_fptr, "%s\n", prev_item);
                 }
             }
-            for(i=0; i<STACK_SIZE; ++i)
-                free(stack[i]);
+            for (i = 0; i < STACK_SIZE; ++i) free(stack[i]);
         }
     }
     fclose(ip_fptr);
--- Tokenizer/main.c
+++ Tokenizer/main.c
@@ -1,26 +1,30 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 #define FILENAME_SIZE 50
 extern char *optarg;
 
 int main(int argc, char *const argv[]) {
-    enum lang {ASCII, English,};
+    enum lang {
+        ASCII,
+        English,
+    };
     enum lang ip_lang = ASCII;
     char ip_file[FILENAME_SIZE], op_file[FILENAME_SIZE];
-    memset(ip_file, '\0', sizeof(ip_file));
-    memset(op_file, '\0', sizeof(op_file));
+    // Linux specific
+    snprintf(ip_file, sizeof(ip_file), "%s", "/dev/stdin");
+    snprintf(op_file, sizeof(op_file), "%s", "/dev/stdout");
     const char *optstring = "l:i:o:";
     int option;
-    while((option = getopt(argc, argv, optstring)) != -1){
-        switch(option){
+    while ((option = getopt(argc, argv, optstring)) != -1) {
+        switch (option) {
             case 'l':
-                if(strcmp(optarg, "English") == 0)
+                if (strcmp(optarg, "English") == 0)
                     ip_lang = English;
                 else if (strcmp(optarg, "ASCII") == 0)
-                    ip_lang = ASCII;                
+                    ip_lang = ASCII;
                 break;
             case 'i':
                 snprintf(ip_file, sizeof(ip_file), "%s", optarg);
@@ -31,43 +35,39 @@ int main(int argc, char *const argv[]) {
         }
     }
     FILE *ip_fptr, *op_fptr;
-    if((ip_fptr = fopen(ip_file,"r")) == NULL){
+    if ((ip_fptr = fopen(ip_file, "r")) == NULL) {
         printf("Cannot open input file\n");
         return 1;
     }
-    op_fptr = fopen(op_file,"w");
-    if((op_fptr = fopen(op_file,"w")) == NULL){
+    op_fptr = fopen(op_file, "w");
+    if ((op_fptr = fopen(op_file, "w")) == NULL) {
         printf("Cannot open output file\n");
         return 1;
     }
-    if(ip_lang == ASCII){
+    if (ip_lang == ASCII) {
         char ch;
-        while (fscanf(ip_fptr, "%c", &ch) != EOF){
+        while (fscanf(ip_fptr, "%c", &ch) != EOF) {
             fprintf(op_fptr, "%c\n", ch);
         }
-    }
-    else if(ip_lang == English){
+    } else if (ip_lang == English) {
         const int MAX_TOKEN_LENGTH = 100;
         char token[MAX_TOKEN_LENGTH + 1], ch;
         int i = 0;
-        while (fscanf(ip_fptr, "%c", &ch) != EOF){
-            if(isalpha(ch)){
-                if(i < sizeof(token) - 1){
+        while (fscanf(ip_fptr, "%c", &ch) != EOF) {
+            if (isalpha(ch)) {
+                if (i < sizeof(token) - 1) {
                     token[i++] = ch;
-                }
-                else{
+                } else {
                     token[sizeof(token) - 1] = '\0';
                     fprintf(op_fptr, "%s\n", token);
                     token[0] = ch;
                     i = 1;
                 }
-            }
-            else if(i > 0){
+            } else if (i > 0) {
                 token[i] = '\0';
                 fprintf(op_fptr, "%s\n%c\n", token, ch);
                 i = 0;
-            }
-            else{
+            } else {
                 fprintf(op_fptr, "%c\n", ch);
             }
         }
