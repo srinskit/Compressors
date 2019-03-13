6b4798bac19af1ef5cfe030f48a8eb14a3bd65cf
srinskit <srinagrao2007@gmail.com>
2019-03-13 11:13:31 +0530

    Added de-tokenizer for ASCII & English

--- Tokenizer/main.c
+++ Tokenizer/main.c
@@ -7,6 +7,7 @@
 extern char *optarg;
 
 int main(int argc, char *const argv[]) {
+    int tokenize = 1;
     enum lang {
         ASCII,
         English,
@@ -16,7 +17,7 @@ int main(int argc, char *const argv[]) {
     // Linux specific
     snprintf(ip_file, sizeof(ip_file), "%s", "/dev/stdin");
     snprintf(op_file, sizeof(op_file), "%s", "/dev/stdout");
-    const char *optstring = "l:i:o:";
+    const char *optstring = "l:i:o:td";
     int option;
     while ((option = getopt(argc, argv, optstring)) != -1) {
         switch (option) {
@@ -32,6 +33,10 @@ int main(int argc, char *const argv[]) {
             case 'o':
                 snprintf(op_file, sizeof(op_file), "%s", optarg);
                 break;
+            case 't':
+            case 'd':
+                tokenize = (option == 't');
+                break;
         }
     }
     FILE *ip_fptr, *op_fptr;
@@ -44,31 +49,52 @@ int main(int argc, char *const argv[]) {
         printf("Cannot open output file\n");
         return 1;
     }
-    if (ip_lang == ASCII) {
-        char ch;
-        while (fscanf(ip_fptr, "%c", &ch) != EOF) {
-            fprintf(op_fptr, "%c\n", ch);
-        }
-    } else if (ip_lang == English) {
-        const int MAX_TOKEN_LENGTH = 100;
-        char token[MAX_TOKEN_LENGTH + 1], ch;
-        int i = 0;
-        while (fscanf(ip_fptr, "%c", &ch) != EOF) {
-            if (isalpha(ch)) {
-                if (i < sizeof(token) - 1) {
-                    token[i++] = ch;
-                } else {
-                    token[sizeof(token) - 1] = '\0';
-                    fprintf(op_fptr, "%s\n", token);
-                    token[0] = ch;
-                    i = 1;
-                }
-            } else if (i > 0) {
-                token[i] = '\0';
-                fprintf(op_fptr, "%s\n%c\n", token, ch);
-                i = 0;
-            } else {
+    if (tokenize) {
+        if (ip_lang == ASCII) {
+            char ch;
+            while (fscanf(ip_fptr, "%c", &ch) != EOF) {
                 fprintf(op_fptr, "%c\n", ch);
+            }
+        } else if (ip_lang == English) {
+            const int MAX_TOKEN_LENGTH = 100;
+            char token[MAX_TOKEN_LENGTH + 1], ch;
+            int i = 0;
+            while (fscanf(ip_fptr, "%c", &ch) != EOF) {
+                if (isalpha(ch)) {
+                    if (i < sizeof(token) - 1) {
+                        token[i++] = ch;
+                    } else {
+                        token[sizeof(token) - 1] = '\0';
+                        fprintf(op_fptr, "%s\n", token);
+                        token[0] = ch;
+                        i = 1;
+                    }
+                } else if (i > 0) {
+                    token[i] = '\0';
+                    fprintf(op_fptr, "%s\n%c\n", token, ch);
+                    i = 0;
+                } else {
+                    fprintf(op_fptr, "%c\n", ch);
+                }
+            }
+        }
+    } else {
+        if (ip_lang == ASCII) {
+            char line[3];
+            while (fgets(line, sizeof(line), ip_fptr) != NULL) {
+                if (line[0] == '\n')
+                    if (fgets(line, sizeof(line), ip_fptr) == NULL) break;
+                fprintf(op_fptr, "%c", line[0]);
+            }
+        } else if (ip_lang == English) {
+            const int MAX_TOKEN_LENGTH = 100;
+            char token[MAX_TOKEN_LENGTH + 1], ch;
+            char line[MAX_TOKEN_LENGTH + 1 + 1];
+            while (fgets(line, sizeof(line), ip_fptr) != NULL) {
+                line[strlen(line) - 1] = '\0';
+                if (strlen(line) == 0)
+                    if (fgets(line, sizeof(line), ip_fptr) == NULL) break;
+                fprintf(op_fptr, "%s", line);
             }
         }
     }
